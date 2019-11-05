6cd1419dcda9e8f38cc96ac89d0825298582e2b0
arbyman <bardelorean@yandex.ru>
2019-11-05 20:58:11 +0300

    hide redux dev

--- src/app.jsx
+++ src/app.jsx
@@ -38,7 +38,7 @@ export default ({ channels, currentChannelId, messages }) => {
     compose(
       applyMiddleware(thunk),
       // eslint-disable-next-line no-underscore-dangle
-      window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+      // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
     ),
   );
   const container = document.getElementById('chat');
